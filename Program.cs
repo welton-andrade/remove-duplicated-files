a2d9d00ad75f0e2915ae63ea0b77104118de3128
Welton <welton.andrade@gmail.com>
2023-02-20 09:27:56 -0300

    refact code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ if(args.Length < 1)
 ICollection<string> files = Directory.GetFiles(args[0]);
 List<string> listPath = new List<string>();
 
-
 foreach (string filePath in files)
 {
     var checksun = SHA256CheckSum(filePath);
@@ -14,12 +13,9 @@ foreach (string filePath in files)
         listPath.Add(checksun);
     else {
         File.Delete(filePath);
-        // File.Move(filePath, @"C:\Temp\" + Path.GetFileName(filePath));
         string? found = listPath.Find(x => x == checksun);
-        System.Console.WriteLine("Found file {0} - {1}", filePath, found);
         System.Console.WriteLine("Remove file {0} - {1}", filePath, checksun);
     }
-    // System.Console.WriteLine(temp._CheckSun);
 }
 
 
